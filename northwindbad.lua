7f806d885fcaf942e7fae5ee6b3028508e8ffb72
Bura1458 <72616872+Bura1458@users.noreply.github.com>
2024-03-25 15:57:45 +0400

    Update northwindbad.lua

--- northwindbad.lua
+++ northwindbad.lua
@@ -1425,7 +1425,6 @@ end
             if closestlootplayerts then
             for index,item in pairs(itemsframeslootcrollvar:GetChildren()) do
                 if item:IsA("Frame") and item:FindFirstChild("Flare") then
-		print("looting player")
                     lootrequestremotets:InvokeServer(closestlootplayerts,item.Name)
                 end
                 end
