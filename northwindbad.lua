e14a0e49f26e67a80a326d203b8584c23fdb0c3b
Bura1458 <72616872+Bura1458@users.noreply.github.com>
2024-03-25 15:01:39 +0400

    Update northwindbad.lua

--- northwindbad.lua
+++ northwindbad.lua
@@ -1,5 +1,12 @@
---// script made by bura 
---// if you wish to get in touch u can add me up my user is dx95 
+--[[
+    This script was made by Bura.
+
+    Tight Studios has been existing for over 2-3 years but due to the recent updates, the key system has been removed and I decided to keep the script open-sourced for everyone to use.
+
+    If you wish to get in touch, you can add me on Discord (dx95).
+
+    - Bura
+--]]
 
 
 
