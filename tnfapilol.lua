6e4f67564bb8c8ad612715a2fc278d0566e8def4
Bura1458 <72616872+Bura1458@users.noreply.github.com>
2024-04-03 23:57:49 +0400

    Update tnfapilol.lua

--- tnfapilol.lua
+++ tnfapilol.lua
@@ -192,7 +192,7 @@ function tightstudioscoolapia:minigungobrrr(state)
 end 
 
 function tightstudioscoolapia:sendtoxicmessage(skiduser)
-local toxicwords = {"nv is obese","nv is skid","bura on top","tightstudios on top  | gg/bcT9w8r","nice anti cheat","jeeper creeper sends his regards","L "..skiduser}
+local toxicwords = {"nv is obese","nv is skid","new age on top","tightstudios on top  | gg/bcT9w8r","nice anti cheat","jeeper creeper sends his regards","L "..skiduser}
 misctable.request("sendChat",toxicwords[math.random(1,#toxicwords)],false)
 end
 
