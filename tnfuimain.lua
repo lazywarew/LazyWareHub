b24608f1600e3fcda6dbcde5d10db2e0db0ea14d
Bura1458 <72616872+Bura1458@users.noreply.github.com>
2024-04-06 22:32:30 +0400

    Update tnfuimain.lua

--- tnfuimain.lua
+++ tnfuimain.lua
@@ -398,7 +398,7 @@ while wait() do
 if infstam == true then
 tightstudioscoolapia:changestaminacapacity(tonumber(100))
 end 
-if autoheal == true and game:GetService('Players').LocalPlayer.Status.Health.Value<100 and  game:GetService('Players').LocalPlayer.Status.IsDead.Value~=true then
+if autoheal == true and inventorytable.itemDrawn == nil and game:GetService('Players').LocalPlayer.Status.Health.Value<100 and  game:GetService('Players').LocalPlayer.Status.IsDead.Value~=true then
 pcall(function()
    if autohealdelay ~= tonumber(0) then
     task.wait(autohealdelay)
