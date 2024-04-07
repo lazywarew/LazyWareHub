d2a3c51af58e6b6b0c4d01e31b86c0d85bfab520
Bura1458 <72616872+Bura1458@users.noreply.github.com>
2024-04-07 14:45:34 +0400

    Update tnfapilol.lua

--- tnfapilol.lua
+++ tnfapilol.lua
@@ -314,7 +314,7 @@ function tightstudioscoolapia:playbandageuseanimation(player)
  local animation = Instance.new("Animation")
 animation.AnimationId = "rbxassetid://" .. dances[math.random(1, #dances)]
 local bandageuseanimationtrack = player.Character:FindFirstChildWhichIsA("Humanoid"):LoadAnimation(usebandageanimationid)
-danceTrack:Play()
+bandageuseanimationtrack:Play()
 end 
 
 
