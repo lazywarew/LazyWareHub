7414fd8b8778f438ec9401568593a37b0d80f082
Bura1458 <72616872+Bura1458@users.noreply.github.com>
2024-04-07 19:48:08 +0400

    Update tnfuimain.lua

--- tnfuimain.lua
+++ tnfuimain.lua
@@ -142,7 +142,7 @@ task.wait(2)
 local Material
 
 xpcall(function()
-Material = loadstring(httrest({Url="https://raw.githubusercontent.com/Bura1458/Script/main/module.lua", Method="GET"}).Body)()
+Material = loadstring(httrest({Url="https://raw.githubusercontent.com/Bura1458/Script/main/tnfapilol.lua", Method="GET"}).Body)()
 end,function()
 while true do end
 return
