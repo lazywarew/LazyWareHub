e2dd4530afc144b283eabbf3521be17ba5aa96a6
Bura1458 <72616872+Bura1458@users.noreply.github.com>
2024-04-29 16:57:35 +0400

    Update boogaloader.lua

--- boogabooga/boogaloader.lua
+++ boogabooga/boogaloader.lua
@@ -1,10 +1,5 @@
-local function import(url)
-    local formattedurl =  loadstring(game:HttpGet('https://raw.githubusercontent.com/Bura1458/Script/'..url))()
-end
-
-
 if game.PlaceId == 11729688377  then
-import("/main/boogabooga/boogamain.lua")
+importmodulets("/main/boogabooga/boogamain.lua")
 else
-import("/main/boogabooga/boogavoid.lua")
+importmodulets("/main/boogabooga/boogavoid.lua")
 end
