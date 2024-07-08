923e22720912b49f79ab9f5ef18f77dcbbd94d28
lazywarew <72616872+lazywarew@users.noreply.github.com>
2024-07-08 14:50:01 +0400

    Update boogaloader.lua

--- boogabooga/boogaloader.lua
+++ boogabooga/boogaloader.lua
@@ -1,5 +1,10 @@
+local plrservicets = game:GetService("Players")
+local me = plrservicets.LocalPlayer
+
 if game.PlaceId == 11729688377  then
 importmodulets("/main/boogabooga/boogamain.lua")
-else
+elseif game.PlaceId == 11879754496 then 
 importmodulets("/main/boogabooga/boogavoid.lua")
+else
+me:Kick("invalid game")
 end
